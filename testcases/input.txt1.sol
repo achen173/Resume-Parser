1a28b7b0144c9a2d677ed7f9ecd2ed38ae037bfa
achen173 <54816128+achen173@users.noreply.github.com>
2019-12-26 22:34:36 -0500

    file cleaning 12/26/2019

--- testcases/input.txt1.sol
+++
@@ -1,6 +0,0 @@
-86 David_Griffon
-26 Joe_Miller
-20 adrian_banks.cv
-18 Paul_Smith.doc
-6 NEO
-4 Richard_Anderson
--- testcases/input.txt2.out
+++
@@ -1,6 +0,0 @@
-45 NEO
-36 David_Griffon
-10 Joe_Miller
-7 Paul_Smith.doc
-2 Richard_Anderson
-2 adrian_banks.cv
